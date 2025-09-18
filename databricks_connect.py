68fbafc34a11c4c6496799a6192bc9b5db28fd98
David Eakin <dje87@scarletmail.rutgers.edu>
2025-09-18 01:11:53 +0000

    Working query!

--- databricks_connect.py
+++ databricks_connect.py
@@ -1,10 +1,10 @@
-#Make Databricks connection
-from databricks.connect import DatabricksSession
-import os
-token = open("secrets/token").read()
-URL = open("secrets/URL").read()
 #Explicit configuration
 def connect_explicit():
+    """Make Databricks connection"""
+    from databricks.connect import DatabricksSession
+    import os
+    token = open("secrets/token").read()
+    URL = open("secrets/URL").read()
     """Connect with explicit configuration"""
     spark = (DatabricksSession.builder
              .host(URL)
